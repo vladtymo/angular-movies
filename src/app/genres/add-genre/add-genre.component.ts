24242479aa3e5567700955a27c781458ec0007f8
vladtymo <tymo.vlad@gmail.com>
2022-04-01 19:28:50 +0300

    Added the component to view and create a genre. Used Reactive Forms and Validators.

---
+++ src/app/genres/add-genre/add-genre.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-add-genre',
+  templateUrl: './add-genre.component.html',
+  styleUrls: ['./add-genre.component.css']
+})
+export class AddGenreComponent implements OnInit {
+
+  //fb: FormBuilder;
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    //this.fb = formBuilder;
+   }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      name: ['', Validators.compose([Validators.required, Validators.email])]
+    });
+  }
+
+  submit(): void {
+
+    if(this.form.controls["name"].errors)
+      alert("Invalid data!");
+    else
+      alert(this.form.controls["name"].value);
+
+    // Saving data...
+  }
+}
